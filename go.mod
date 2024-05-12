e5ebed866b1dacce5208091b2af1b21610d36aa9
Wim Henderickx <wim.henderickx@gmail.com>
2024-05-12 13:27:10 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/kform-dev/kform-plugin
+
+go 1.22.2
+
+require (
+	github.com/henderiw/logger v0.0.0-20230911123436-8655829b1abe
+	github.com/kform-dev/plugin v0.0.0-20240512102056-3e4cbfad1f6e
+	google.golang.org/grpc v1.63.2
+	google.golang.org/protobuf v1.34.1
+)
+
+require (
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/oklog/run v1.1.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
+)
---
+++ kfprotov1/provider.go
@@ -0,0 +1,28 @@
+package kfprotov1
+
+import (
+	"context"
+
+	"github.com/kform-dev/kform-plugin/kfprotov1/kfplugin1"
+)
+
+type ProviderServer interface {
+	Capabilities(ctx context.Context, in *kfplugin1.Capabilities_Request) (*kfplugin1.Capabilities_Response, error)
+	Configure(ctx context.Context, in *kfplugin1.Configure_Request) (*kfplugin1.Configure_Response, error)
+	StopProvider(ctx context.Context, in *kfplugin1.StopProvider_Request) (*kfplugin1.StopProvider_Response, error)
+
+	DataSourceServer
+	ResourceServer
+}
+
+type DataSourceServer interface {
+	ReadDataSource(ctx context.Context, in *kfplugin1.ReadDataSource_Request) (*kfplugin1.ReadDataSource_Response, error)
+	ListDataSource(ctx context.Context, in *kfplugin1.ListDataSource_Request) (*kfplugin1.ListDataSource_Response, error)
+}
+
+type ResourceServer interface {
+	ReadResource(ctx context.Context, in *kfplugin1.ReadResource_Request) (*kfplugin1.ReadResource_Response, error)
+	CreateResource(ctx context.Context, in *kfplugin1.CreateResource_Request) (*kfplugin1.CreateResource_Response, error)
+	UpdateResource(ctx context.Context, in *kfplugin1.UpdateResource_Request) (*kfplugin1.UpdateResource_Response, error)
+	DeleteResource(ctx context.Context, in *kfplugin1.DeleteResource_Request) (*kfplugin1.DeleteResource_Response, error)
+}
